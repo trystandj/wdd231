a26ed7ecd499ed3e1456b6373f8c5d2f531d655b
trystandj <trystandj@gmail.com>
2025-03-08 16:15:33 -0500

    finished

---
+++ scripts/test.js
@@ -0,0 +1,3 @@
+const daysUntil = "There are " + daycount + " days until " + targetEvent + "!";
+const days1Until = `There are  ${daycount}  days until ${targetEvent} !`;
+today2.innerHTML = `<strong>Volume</strong>: ${volume} liters`; 
