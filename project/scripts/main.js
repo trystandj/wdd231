d20dc6a95831ec4465c9c5268ca91d483426c916
trystandj <trystandj@gmail.com>
2025-04-10 19:05:55 -0400

    links

--- project/scripts/main.js
+++ project/scripts/main.js
@@ -1,5 +1,6 @@
 import { fetchData } from './api.js';
 import { displayRandomQuote } from './quoteDisplay.js';
+import { displayRandomWorkout } from './workoutPlan.js';
 
 const quoteURL = 'https://trystandj.github.io/wdd231/project/data/wellness-qoutes.json';
 
@@ -7,6 +8,18 @@ async function init() {
     const data = await fetchData(quoteURL);
     if (data && data.qoutes) {
         displayRandomQuote(data.qoutes); 
+    }
+    else {
+        console.error("No quotes data found.");
+    }
+    // Fetch and display random workouts
+    const workoutURL = 'https://trystandj.github.io/wdd231/project/data/workout-plans.json';
+    const workoutData = await fetchData(workoutURL);
+    if (workoutData && workoutData.workouts) {
+      displayRandomWorkout(workoutData.workouts); 
+    }
+    else {
+        console.error("No workouts data found.");
     }
 }
 
